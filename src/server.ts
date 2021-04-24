3859ae9ce61aa81a11fe8f45cd3239fdc73cab8a
Eduardo Junior <goldblade@gmail.com>
2021-04-24 20:24:42 -0300

    first config

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Olá NLW 05'
+  })
+})
+
+app.post('/users', (req, res) => {
+  return res.json({ message: 'Usuário salvo com sucesso' })
+})
+
+app.listen(3333, () => console.log('Server is running on port 3333'))
